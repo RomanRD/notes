bc1351ae5f5370eefef9cdb1f715bad58c2e961b
romgor <romanhorkoon@gmail.com>
2023-08-06 18:11:45 +0300

    Initialize users on the first run of the project

--- src/main/java/com/service/notes/NotesApplication.java
+++ src/main/java/com/service/notes/NotesApplication.java
@@ -1,9 +1,11 @@
 package com.service.notes;
 
+import com.github.cloudyrock.spring.v5.EnableMongock;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableMongock
 public class NotesApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/service/notes/config/dbmigrations/DatabaseInitChangeLog.java
@@ -0,0 +1,42 @@
+package com.service.notes.config.dbmigrations;
+
+import com.github.cloudyrock.mongock.ChangeLog;
+import com.github.cloudyrock.mongock.ChangeSet;
+import com.service.notes.model.Role;
+import com.service.notes.persistence.entity.User;
+import com.service.notes.persistence.repository.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.List;
+
+@ChangeLog(order = "001")
+public class DatabaseInitChangeLog {
+
+    @ChangeSet(order = "001", id = "init_users", author = "roman")
+    public void initDepartments(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+
+        User user = new User();
+        user.setFirstName("FirstNameUser");
+        user.setSecondName("SecondNameUser");
+        user.setUsername("userTest");
+        user.setPassword(passwordEncoder.encode("userTest"));
+        user.setRoles(List.of(Role.USER));
+
+        User admin = new User();
+        admin.setFirstName("FirstNameAdmin");
+        admin.setSecondName("SecondNameAdmin");
+        admin.setUsername("admin");
+        admin.setPassword(passwordEncoder.encode("admin"));
+        admin.setRoles(List.of(Role.ADMIN));
+
+        User userAdmin = new User();
+        userAdmin.setFirstName("FirstNameUserAdmin");
+        userAdmin.setSecondName("SecondNameUserAdmin");
+        userAdmin.setUsername("userAdmin");
+        userAdmin.setPassword(passwordEncoder.encode("userAdmin"));
+        userAdmin.setRoles(List.of(Role.USER, Role.ADMIN));
+
+        userRepository.saveAll(List.of(user, admin, userAdmin));
+    }
+
+}
